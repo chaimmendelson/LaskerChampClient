a5b709c86ee4910e40f465e1e3eacd8fc65fc19a
HTMLess <74318873+HTMLess@users.noreply.github.com>
2022-12-01 12:37:01 +0200

    ...

--- src/login_page.js
+++ src/login_page.js
@@ -46,31 +46,29 @@ let log_in_info = {'test': 'test'}
 let rating = 1200
 
 //submit function
-document.getElementById('submit').addEventListener("click", function() {
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    document.getElementById('username').style.border = '1px solid black';
-    document.getElementById('password').style.border = '1px solid black';
-    if (!checkUsername(username)){
-        document.getElementById('username').style.border = '2px solid red';
-        return;
+document.getElementById("submit").addEventListener("click", async function () { // this function will run when the user click on the login button
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var user = {username: username, password: password};
+    let url = 'http://localhost:8000/login'; // the url to send the request to
+    const response  = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user),
+    });
+    console.log(response);
+    if (!response.status === 200) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
     }
-    if (!checkPassword(password)){
-        document.getElementById('password').style.border = '2px solid red';
-        return;
-    }
-    if (username in log_in_info){
-        if (log_in_info[username] == password){
-            console.log('success')
-            // window.location.replace('http://192.168.43.133:8000/') // doesn't successfully redirect, it takes far to long
-        }
-        else {
-            console.log('incorrect password');
-            document.getElementById('password').style.border = '2px solid red';
-        }
-    }
-    else {
-        console.log('incorrect username');
-        document.getElementById('username').style.border = '2px solid red';
-    }
+    const data = await response.json();
+    if (data.status === 'success') {
+        localStorage.setItem('chess-cookie', data.cookie);
+        let url = data.redirect;
+        window.location = url;
+    };
+    console.log(data.status);
 });
