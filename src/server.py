a2c233d7e098314c3e090a5a0fe9e98b739632d0
chaim <chaimke2005@gmail.com>
2023-03-01 13:06:19 +0200

    change port to 80

--- src/server.py
+++ src/server.py
@@ -253,7 +253,7 @@ def main():
                               args=(lambda: stop_thread,))
     try:
         thread.start()
-        web.run_app(app, port=5678)
+        web.run_app(app, port=80)
     except KeyboardInterrupt:
         pass
     stop_thread = True
